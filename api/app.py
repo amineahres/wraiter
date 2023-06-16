a6590c0ba709660d602ca96fec6a4af1162038a6
Amine <amine.ahres@gmail.com>
2023-06-16 16:05:03 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,25 +1,34 @@
 from flask import Flask, request, render_template
-import openai
 import requests
 import os
+import json
 
 app = Flask(__name__, template_folder=os.path.abspath('templates'))
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         user_input = request.form['user_input']
-        # Use OpenAI's API to get the response from ChatGPT
-        openai.api_key = os.environ.get('OPENAI_API_KEY')
-        print(openai.api_key) 
-        response = openai.Completion.create(engine='davinci-codex', prompt=user_input, max_tokens=100)
-        chatgpt_response = response.choices[0].text
 
-        # Save data to Supabase
-        #supabase_url = 'https://qkhixorvlsmwhfjuflqm.supabase.co'
-        #supabase_headers = {'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFraGl4b3J2bHNtd2hmanVmbHFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY4NDE4NjQsImV4cCI6MjAwMjQxNzg2NH0.6g8qVcUscMlN2GpCG6sDm2LOZngnKGyQ0moEiZaWK8g', 'Content-Type': 'application/json'}
-        #payload = {"user_input": user_input, "chatgpt_response": chatgpt_response}
-        #requests.post(supabase_url, headers=supabase_headers, json=payload)
+        # Use OpenAI's API to get the response from ChatGPT
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {OPENAI_API_KEY}',
+        }
+
+        data = {
+            'engine': 'davinci-codex',
+            'prompt': user_input,
+            'max_tokens': 100
+        }
+
+        response = requests.post('https://api.openai.com/v1/engines/davinci-codex/completions', headers=headers, data=json.dumps(data))
+        chatgpt_response = response.json()['choices'][0]['text']
+
+        # Save data to Supabase (This part remains unchanged)
+        # ... code to save data to Supabase ...
 
         return render_template('index.html', chatgpt_response=chatgpt_response)
 
