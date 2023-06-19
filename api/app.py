da01e4e2dfb97d9b266f0b10b223a3434e942340
Amine <amine.ahres@gmail.com>
2023-06-19 14:37:26 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -23,54 +23,54 @@ def index():
         if content is None or len(content) < 10:
             chatgpt_result = "Please fill in the content with at least 5 characters."
         else:
-        # Create prompt
-        prompt = ('Find between percent signs an input. from that input, please create a ' + content_type + 
-        ' which has the following characteristics: \n' +
-        '- Tone: ' + content_tone + ' \n' +
-        '- Length: ' + length + ' \n' +
-        'and take into account the following request: ' + context + ' \n' +
-        '% \n' +
-        content + 
-        '\n%')
-        
-        print(prompt)
-
-        # Use OpenAI's API to get the response from ChatGPT
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {OPENAI_API_KEY}',
-        }
-
-        messages = [
-            {"role": "user", "content": prompt}
-        ]
-
-        # Send the API request to ChatGPT
-        chatgpt_response = requests.post(
-            chatgpt_api_url,
-            headers=headers,
-            json={
-                "messages": messages,
-                "model": "gpt-3.5-turbo",
-                "max_tokens": 600,
-                "temperature": 0.5
+            # Create prompt
+            prompt = ('Find between percent signs an input. from that input, please create a ' + content_type + 
+            ' which has the following characteristics: \n' +
+            '- Tone: ' + content_tone + ' \n' +
+            '- Length: ' + length + ' \n' +
+            'and take into account the following request: ' + context + ' \n' +
+            '% \n' +
+            content + 
+            '\n%')
+            
+            print(prompt)
+    
+            # Use OpenAI's API to get the response from ChatGPT
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': f'Bearer {OPENAI_API_KEY}',
             }
-        )
-
-        # Handle the ChatGPT response and extract the adapted query
-        chatgpt_result = None
-
-        
-        if chatgpt_response.status_code == 200:
-            chatgpt_result = chatgpt_response.json()
-            chatgpt_result = chatgpt_result["choices"][0]["message"]["content"]
-        else:
-            print(chatgpt_response.status_code)
-            print(chatgpt_response.content)
-            chatgpt_result = "Error: Failed to receive response from ChatGPT"
-
-        return render_template('index.html', chatgpt_response=chatgpt_result)
-
-    return render_template('index.html', chatgpt_response=None)
+    
+            messages = [
+                {"role": "user", "content": prompt}
+            ]
+    
+            # Send the API request to ChatGPT
+            chatgpt_response = requests.post(
+                chatgpt_api_url,
+                headers=headers,
+                json={
+                    "messages": messages,
+                    "model": "gpt-3.5-turbo",
+                    "max_tokens": 600,
+                    "temperature": 0.5
+                }
+            )
+    
+            # Handle the ChatGPT response and extract the adapted query
+            chatgpt_result = None
+    
+            
+            if chatgpt_response.status_code == 200:
+                chatgpt_result = chatgpt_response.json()
+                chatgpt_result = chatgpt_result["choices"][0]["message"]["content"]
+            else:
+                print(chatgpt_response.status_code)
+                print(chatgpt_response.content)
+                chatgpt_result = "Error: Failed to receive response from ChatGPT"
+    
+            return render_template('index.html', chatgpt_response=chatgpt_result)
+    
+        return render_template('index.html', chatgpt_response=None)
 
 # Note: You don't need to have app.run() here as Vercel will handle running the application
