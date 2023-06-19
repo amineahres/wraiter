0af0dd2049cf4ba9da2b79c58535c75aff23fd13
Amine <amine.ahres@gmail.com>
2023-06-19 16:55:43 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 import requests
 import os
 import json
-from postgrest_py import Postgrest
+from postgrest-py import Postgrest
 
 app = Flask(__name__, template_folder=os.path.abspath('templates'))
 
