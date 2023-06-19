d0336e4dc42320b5895f4ad915ae5e5abd8415fd
Amine <amine.ahres@gmail.com>
2023-06-19 16:47:32 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,13 +2,13 @@ from flask import Flask, request, render_template
 import requests
 import os
 import json
-from supabase_py import create_client, Client
+from postgrest_py import Postgrest
 
 app = Flask(__name__, template_folder=os.path.abspath('templates'))
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-SUPABASE_URL = 'https://qkhixorvlsmwhfjuflqm.supabase.co'
-SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFraGl4b3J2bHNtd2hmanVmbHFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY4NDE4NjQsImV4cCI6MjAwMjQxNzg2NH0.6g8qVcUscMlN2GpCG6sDm2LOZngnKGyQ0moEiZaWK8g'
+SUPABASE_URL = os.environ.get('SUPABASE_URL')
+SUPABASE_KEY = os.environ.get('SUPABASE_KEY')
 chatgpt_api_url = "https://api.openai.com/v1/chat/completions"
 
 @app.route('/', methods=['GET', 'POST'])
@@ -78,16 +78,15 @@ def index():
             return render_template('index.html', chatgpt_response=chatgpt_result)
     
     # Save data to Supabase
-    data = {
-        "input_type": input_type,
-        "input_tone": input_tone,
-        "input_length": input_length,
-        "input_context": input_context,
-        "input_content": input_content,
-        "output_prompt": output_prompt,
-        "output_result": output_result 
-    }
-    supabase.table('user_inputs').insert(data)
+    supabase.insert({
+        'input_type': input_type,
+        'input_tone': input_tone,
+        'input_length': input_length,
+        'input_context': input_context,
+        'input_content': input_content,
+        'output_prompt': output_prompt,
+        'output_result': output_result 
+    })
     
     return render_template('index.html', chatgpt_response=None)
 
